c8d18dd9fe588372abbca4af2aab9b3358e5df64
Advay Sanketi <advay2807@gmail.com>
2026-07-26 19:31:56 +0530

    Remove unused UI components

--- src/app/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { Header } from '@/app/components/Header'
-export { Footer } from '@/app/components/Footer'
-export { ProjectCard } from '@/app/components/ProjectCard'
-export { HeadingLink } from '@/app/components/HeadingLink'
